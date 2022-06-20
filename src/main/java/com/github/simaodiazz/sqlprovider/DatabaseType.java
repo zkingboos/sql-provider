71909ba3648e1edbb242f2abbeb7192ef4d86f69
zkingboos <kingboosoficial@gmail.com>
2022-06-20 16:23:39 -0300

    style: format DatabaseType enum

--- src/main/java/com/github/simaodiazz/sqlprovider/DatabaseType.java
+++ src/main/java/com/github/simaodiazz/sqlprovider/DatabaseType.java
@@ -3,11 +3,14 @@ package com.github.simaodiazz.sqlprovider;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
+@AllArgsConstructor
 public enum DatabaseType {
 
-    MARIADB("MariaDB"),MYSQL("MySQL"),POSTGRESQL("PostgreSQL"),SQLITE("SQLite");
+    MARIADB("MariaDB"),
+    MYSQL("MySQL"),
+    POSTGRESQL("PostgreSQL"),
+    SQLITE("SQLite");
 
     private final String name;
 
